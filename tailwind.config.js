980031d700e318570e345bac5206e5871b90e300
slicepugsnips-cmd <slicepugsnips@gmail.com>
2026-01-01 14:18:34 -0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF5F1F",
+      },
+    },
+  },
+  plugins: [],
+}
