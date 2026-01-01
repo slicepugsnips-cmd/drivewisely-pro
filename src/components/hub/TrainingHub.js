8c43d7f3dbf0413cd7fcf230302dca2967f0c7b0
slicepugsnips-cmd <slicepugsnips@gmail.com>
2026-01-01 15:20:34 -0800

    Update TrainingHub.js

--- src/components/hub/TrainingHub.js
+++ src/components/hub/TrainingHub.js
@@ -1,3 +1,4 @@
+'use client';
 import React, { useState, useEffect } from 'react';
 import { collection, getDocs, query, orderBy } from 'firebase/firestore';
 import { db } from '../../lib/firebase';
